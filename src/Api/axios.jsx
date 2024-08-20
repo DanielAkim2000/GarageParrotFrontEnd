7a857feca77f9d04a166105d01c21c4853dd9532
DanielAkim2000 <emanedanielakim@gmail.com>
2024-08-21 00:47:53 +0200

    ajout de baseUrl

--- src/Api/axios.jsx
+++ src/Api/axios.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://garagevparrotakim-72113a77a6a7.herokuapp.com/",
+  baseURL: "https://backgarage-005855bd43c7.herokuapp.com/",
 });
